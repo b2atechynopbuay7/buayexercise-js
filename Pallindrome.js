feeae645482400d6a3327600d8ff66b26712f5f4
b2atechynopbuay7 <85606489+b2atechynopbuay7@users.noreply.github.com>
2021-07-03 10:38:19 +0400

    add js exercise palindrom

---
+++ Pallindrome.js
@@ -0,0 +1,34 @@
+
+function Palindrome(entry){
+   
+       var formated_string = entry.toLowerCase();
+        var counter = 0;
+   
+        //if entered string is empty return false
+        if(formated_string ==="") {
+            return false;
+        }
+    
+        if ((formated_string .length) % 2 === 0) {
+            counter = (formated_string .length) / 2;
+        } else {
+    // If the length of the string is 1 then it becomes a palindrome
+            if (formated_string .length === 1) {
+                console.log("Entry is a palindrome.");
+                return true;
+            } else {
+    // If the length of the string is odd ignore middle character
+                counter = (formated_string .length - 1) / 2;
+            }
+        }
+    // Loop through to check the first character to the last character and then move next
+        for (var x = 0; x < counter; x++) {
+    // Compare characters and drop them if they do not match 
+            if (formated_string [x] != formated_string .slice(-1-x)[0]) {
+                console.log("Entry is not a palindrome.");
+                return false;
+            }
+        }
+        console.log("The entry is a palindrome.");
+        return true;
+    }
